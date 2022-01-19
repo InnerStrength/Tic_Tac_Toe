944ee4c053d4418fc22708e5cb53d5d09d09b992
Patrick Tonkinson <patrickngp@gmail.com>
2022-01-19 01:15:56 -0500

    Add files via upload

---
+++ board_setup.py
@@ -0,0 +1,46 @@
+class Board:
+
+    def __init__(self):
+        self.aa = {"x": "x",
+                   "o": "0",
+                   "hor": '===',
+                   "ver": "|",
+                   }
+        self.board = self.refresh_board()
+        self.remaining_choices = self.remaining_choices()
+        self.winner = ""
+        self.game_is_on = True
+        self.placement = {
+            "T": 0,
+            "M": 2,
+            "R": 4,
+            "L": 0,
+            "B": 4,
+        }
+
+
+        self.winning_sets = [["BL", "BM", "BR"], ["ML", "MR", "MM"], ["TL", "TM", "TR"],
+                             ["TL", "ML", "BL"], ["TM", "MM", "BM"], ["TR", "MR", "BR"],
+                             ["TL", "MM", "BR"], ["TR", "MM", "BL"]]
+
+        self.computer_picks = ["BL", "BM", "BR", "ML", "MM", "MR", "TL", "TM", "TR"]
+
+    def refresh_board(self):
+        return [["   ", self.aa["ver"], "   ", self.aa["ver"], "   ", "\n"],
+                [self.aa["hor"], self.aa["ver"], self.aa["hor"], self.aa["ver"], self.aa["hor"], "\n"],
+                ["   ", self.aa["ver"], "   ", self.aa["ver"], "   ", "\n"],
+                [self.aa["hor"], self.aa["ver"], self.aa["hor"], self.aa["ver"], self.aa["hor"], "\n"],
+                ["   ", self.aa["ver"], "   ", self.aa["ver"], "   ", "\n"]]
+
+    def remaining_choices(self):
+        return "TL TM TR \n " \
+               "ML MM MR \n " \
+               "BL BM BR \n "
+
+    def print_board(self):
+        top = f" {''.join(self.board[0])}"
+        line1 = ''.join(self.board[1])
+        mid = ''.join(self.board[2])
+        line2 = ''.join(self.board[3])
+        bot = ''.join(self.board[4])
+        print(top, line1, mid, line2, bot)
---
+++ main.py
@@ -0,0 +1,116 @@
+from replit import clear
+from board_setup import Board
+from random import choice
+from asciiart import ascii
+
+x_place = []
+o_place = []
+gb = Board()
+clear = "\n" * 100
+
+def check_winner(let):
+    if let == "X":
+        for tic_tac_toe in gb.winning_sets:
+            if all(x in x_place for x in tic_tac_toe):
+                gb.winner = "goes to X!"
+                gb.game_is_on = False
+    else:
+        for tic_tac_toe in gb.winning_sets:
+            if all(o in o_place for o in tic_tac_toe):
+                gb.winner = "goes to O!"
+                gb.game_is_on = False
+
+    chars = set('MBLRT')
+    if any((c in chars) for c in gb.remaining_choices):
+        pass
+    else:
+        gb.print_board()
+        gb.winner = "was a TIE"
+        gb.game_is_on = False
+
+
+def player_go(let):
+    print(clear)
+    gb.print_board()
+    select = input(f"It is {let}'s turn, select a spot:\n "
+                   f"{gb.remaining_choices}").upper()
+    check = True
+    while check:
+        if select not in gb.remaining_choices:
+            gb.print_board()
+            select = input(f"not a valid choice, select again:\n "
+                           f"{gb.remaining_choices}").upper()
+        else:
+            gb.remaining_choices = gb.remaining_choices.replace(f"{select}", "  ")
+            check = False
+    letters = [a for a in select]
+    gb.board[gb.placement[letters[0]]][gb.placement[letters[1]]] = f" {let} "
+    if let == "X":
+        x_place.append(select)
+    else:
+        o_place.append(select)
+
+
+def computer_go(let):
+    computer_choice = choice(gb.computer_picks)
+    check = True
+    while check:
+        if computer_choice not in gb.remaining_choices:
+            computer_choice = choice(gb.computer_picks)
+        else:
+            gb.remaining_choices = gb.remaining_choices.replace(f"{computer_choice}", "  ")
+            check = False
+    letters = [a for a in computer_choice]
+    gb.board[gb.placement[letters[0]]][gb.placement[letters[1]]] = f" {let} "
+    if let == "X":
+        x_place.append(computer_choice)
+    else:
+        o_place.append(computer_choice)
+
+
+def game():
+    global gb
+    global x_place
+    global o_place
+    print(ascii)
+    gb = Board()
+    select_game_type = input("One or Two Players:\n").lower()
+    if select_game_type == "two" or select_game_type == "2":
+        while gb.game_is_on:
+            if gb.game_is_on:
+                player_go("X")
+                check_winner("X")
+            if gb.game_is_on:
+                player_go("O")
+                check_winner("O")
+    else:
+        computer_turn = choice(["X","O"])
+        if computer_turn == "X":
+            while gb.game_is_on:
+                if gb.game_is_on:
+                    computer_go("X")
+                    check_winner("X")
+                if gb.game_is_on:
+                    player_go("O")
+                    check_winner("O")
+        else:
+            while gb.game_is_on:
+                if gb.game_is_on:
+                    player_go("X")
+                    check_winner("X")
+                if gb.game_is_on:
+                    computer_go("O")
+                    check_winner("O")
+    print(clear)
+    print(ascii)
+    print(f"This game {gb.winner}\n")
+    replay = input("Would you like to play again? Y/N\n").lower()
+    if replay == "y" or replay == "yes":
+        print(clear)
+        gb.winner=""
+        o_place = []
+        x_place = []
+        gb.game_is_on = True
+        game()
+
+game()
